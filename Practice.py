f1cb2e8703e14d984dba5b27b95357271c00e54a
Muhammad Hussain <88007126+SYED-M-HUSSAIN@users.noreply.github.com>
2024-02-05 14:29:39 +0500

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -1,1 +1,7 @@
-import tensorflow as ts
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation , Input
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import boston_housing
